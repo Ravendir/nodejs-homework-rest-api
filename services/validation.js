6c963b9d40312486c2c600c87e9deabba51d27f4
Valerian Rykov <Valerra2@gmail.com>
2022-07-09 21:24:45 +0300

    upload

--- services/validation.js
+++ services/validation.js
@@ -1,36 +1,36 @@
 const Joi = require("joi");
 
-const schemaCreateContact = Joi.object({
-  name: Joi.string().min(3).max(30).required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string()
-    .pattern(/^[(][\d]{3}[)]\s[\d]{3}[-][\d]{4}/)
-    .required(),
-});
+const postValidate = (req, res, next) => {
+  const contactSchema = Joi.object({
+    name: Joi.string().min(3).max(30).required(),
+    email: Joi.string().email().required(),
+    phone: Joi.string().min(6).max(15).required(),
+  });
 
-const schemaUpdateContact = Joi.object({
-  name: Joi.string().min(3).max(30).optional(),
-  email: Joi.string().email().optional(),
-  phone: Joi.string()
-    .pattern(/^[(][\d]{3}[)]\s[\d]{3}[-][\d]{4}/)
-    .optional(),
-});
+  const { error } = contactSchema.validate(req.body);
 
-const validate = (schema, obj, next) => {
-  const { error } = schema.validate(obj);
   if (error) {
-    return next({
-      status: 400,
-      message: "Bad request",
-    });
+    return res.status(400).send(error.message);
   }
   next();
 };
 
-module.exports.createContact = (req, _res, next) => {
-  return validate(schemaCreateContact, req.body, next);
+const updateValidate = (req, res, next) => {
+  const contactSchema = Joi.object({
+    name: Joi.string().min(3).max(30),
+    email: Joi.string().min(4).max(30).email(),
+    phone: Joi.string().min(6).max(15),
+  });
+
+  const { error } = contactSchema.validate(req.body);
+
+  if (error) {
+    return res.status(400).send(error.message);
+  }
+  next();
 };
 
-module.exports.updateContact = (req, _res, next) => {
-  return validate(schemaUpdateContact, req.body, next);
+module.exports = {
+  postValidate,
+  updateValidate,
 };
