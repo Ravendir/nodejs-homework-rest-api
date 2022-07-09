ecd597bc4ac9c9a02b5dfa995cf7f36eb1b84637
Valerian Rykov <Valerra2@gmail.com>
2022-07-09 21:29:42 +0300

    upload

--- services/validation.js
+++ services/validation.js
@@ -4,7 +4,7 @@ const postValidate = (req, res, next) => {
   const contactSchema = Joi.object({
     name: Joi.string().min(3).max(30).required(),
     email: Joi.string().email().required(),
-    phone: Joi.string().min(6).max(15).required(),
+    phone: Joi.string().min(6).max(20).required(),
   });
 
   const { error } = contactSchema.validate(req.body);
